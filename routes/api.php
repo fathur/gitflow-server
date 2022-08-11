f33b5b9d388ca57fa8624cf1cfc0092e9e964f31
Fathur Rohman <hi.fathur.rohman@gmail.com>
2022-08-11 16:16:46 +0700

    register routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+# Used to opened and reopened PR
+# Send params 
+# branch=jhg
+Route::post('/sub-domains/{slug}', function (Request $request, $slug) {
+    # Call script to create new virtual environment
+    # - create route 53 subdomain
+    # - create virtual host file
+    # - clone repo at specific branch
+    # - restart nginx
+
+    # Notify in Github issue/PR comment
+
+    return $slug;
+
+});
+
+# Used to closed PR
+# Send params 
+# merged=false
+# branch=jhg
+Route::delete('/sub-domains/{slug}', function (Request $request, $slug) {
+    # Call script to destroy virtual environment
+    # - delete route 53 subdomain
+    # - delete virtual host file
+    # - delete repo at specific slug
+    # - restart nginx
+
+    # Notify in Github issue/PR comment 
+
+    return $slug;
+});
+
+# Used to merged PR
+# Send params 
+# merged=true
+# target_branch=abc
+# branch=jhg
+Route::delete('/sub-domains/{slug}', function (Request $request, $slug) {
+    # Call script to destroy virtual environment
+    # - delete route 53 subdomain
+    # - delete virtual host file
+    # - delete repo at specific slug
+    # - restart nginx
+
+    # Deploy target branch with new code
+    # - pull latest changes
+    # - restart nginx
+
+    # Notify in Github issue/PR comment that PR successfull merged
+
+    return $slug;
+})
+
+# Used to sync PR
+# Send params 
+# branch=jhg
+Route::put('/sub-domains/{slug}', function (Request $request, $slug) {
+    # Deploy branch with new code
+    # - pull latest changes
+    # - restart nginx
+
+    return $slug;
 });
