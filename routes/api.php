48a0dd055639f2967d81be2341ef06ce84cbc659
Fathur Rohman <hi.fathur.rohman@gmail.com>
2022-08-11 19:16:56 +0700

    call github api for comment

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+class Github
+{
+    private $token;
+
+    protected $issueNumber;
+
+    public function __construct($token = null) {
+        if (is_null($token)) {
+            $this->token = "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";
+        } else {
+            $this->token = $token;
+        }
+
+    }
+
+    public function setIssueNumber($number) {
+        $this->issueNumber = $number; 
+        return $this;                             
+    }
+
+    public function comment($comment)
+    {
+        $response = Http::withHeaders([
+            "Authorization" => "token {$this->token}",
+            "Accept" => "application/vnd.github+json"
+        ])->post("https://api.github.com/repos/fathur/gitflow-workflow/issues/{$this->issueNumber}/comments", [
+            "body" => $comment
+        ]);
+    }
+}
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
@@ -21,6 +54,7 @@ use Illuminate\Support\Facades\Route;
 # Used to opened and reopened PR
 # Send params 
 # branch=jhg
+# pr_number=3
 Route::post('/sub-domains/{slug}', function (Request $request, $slug) {
     # Call script to create new virtual environment
     # - create route 53 subdomain
@@ -29,6 +63,8 @@ Route::post('/sub-domains/{slug}', function (Request $request, $slug) {
     # - restart nginx
 
     # Notify in Github issue/PR comment
+    (new Github())->setIssueNumber($request->get("pr_number"))
+        ->comment("Send from gitflow server");
 
     return $slug;
 
@@ -46,6 +82,8 @@ Route::delete('/sub-domains/{slug}', function (Request $request, $slug) {
     # - restart nginx
 
     # Notify in Github issue/PR comment 
+    (new Github())->setIssueNumber($request->get("pr_number"))
+        ->comment("Send from gitflow server");
 
     return $slug;
 });
@@ -67,7 +105,9 @@ Route::delete('/sub-domains/{slug}', function (Request $request, $slug) {
     # - restart nginx
 
     # Notify in Github issue/PR comment that PR successfull merged
-
+    (new Github())->setIssueNumber($request->get("pr_number"))
+        ->comment("Send from gitflow server");
+        
     return $slug;
 });
 
